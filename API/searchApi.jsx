db0671aeb227f053e8c085d72c0a1754e2bbc2e4
TimiMaliki <malikitimi@gmail.com>
2024-04-12 22:04:07 +0100

    finished

---
+++ API/searchApi.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const searchApi = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default searchApi
---
+++ Router.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "./MainLayout";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import Search, { Dogs } from "./pages/Dogs";
+import SingleDog from "./pages/SingleDog";
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/dogs" element={<Dogs/>} />
+        <Route path="/:name" element={<SingleDog/>} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default Router;
